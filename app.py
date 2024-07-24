e2f0b7cfce835dc1018e004f14ba9f71413cda1b
Angelo Franciamore <angelo99franciamore@gmail.com>
2024-07-24 11:28:54 +0200

    chore: Add route for adding a book

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+from flask_font_awesome import FontAwesome
+
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+font_awesome = FontAwesome(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/add')
+def add_book():
+    return render_template('add.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
- 
